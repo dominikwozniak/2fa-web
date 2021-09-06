18b07a72ff80d8889ed3bf057c2374baeb9be4e1
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-09-06 19:43:58 +0200

    BE - add swagger configuration

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOkResponse({
+    description: 'Check alive',
+  })
   checkAlive(): string {
     return this.appService.checkAlive();
   }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,29 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.BACKEND_PORT);
+
+  const options = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('API description')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'Token',
+      },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.BACKEND_PORT || 4000);
 }
 bootstrap();
