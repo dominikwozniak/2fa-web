e399bd520ab54e1f39518fcaafab42548993b0a1
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-09-11 16:33:01 +0200

    BE - register error filter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,19 @@
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingInterceptor } from './shared/logging.interceptor';
+import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
     AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
