12b4a2647b8fd349eab3eeda8d96eee35967a5fc
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-09-12 23:34:04 +0200

    FE - prepare styles

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,6 +17,14 @@ module.exports = {
     'require-jsdoc': 0,
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-first-prop-new-line': [1, 'multiline'],
+    'react/jsx-max-props-per-line': [
+      1,
+      {
+        maximum: 1,
+        when: 'always',
+      },
+    ],
   },
   settings: {
     'import/ignore': ['.css$', 'node_modules/*'],
