a2ee1ee973e1a79c6b14d80f496fbd49d7cd6053
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-09-18 14:13:43 +0200

    FE - add mainTemplate layout

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -2,11 +2,12 @@ import React, { useCallback } from 'react';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import Image from 'next/image';
+import MainTemplate from '@/templates/MainTemplate';
 import img from '@/public/assets/phone_data.svg';
-import { UserSignUpPayload } from "@/types/user.types";
+import { UserSignUpPayload } from '@/types/user.types';
 import ErrorInputIcon from '@/components/ErrorInputIcon';
 
-const Signup = () => {
+const Signup: React.FC = () => {
   const {
     handleSubmit,
     register,
@@ -26,97 +27,99 @@ const Signup = () => {
   );
 
   return (
-    <div className="is-flex is-flex-direction-column signup">
-      <div className="is-flex is-flex-direction-column is-justify-content-center signup__header">
-        <Image src={img} alt={'Signup banner image'} width={250} />
-        <h1>Sign up</h1>
-        <h4>I’m glad that you are joing the group of users</h4>
-      </div>
-      <form
-        className="columns mt-4 mb-0 mx-auto is-flex is-flex-direction-column signup__form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="column p-0">
-          {errors.email && <ErrorInputIcon />}
-          <input
-            type="email"
-            aria-invalid={errors.email ? 'true' : 'false'}
-            placeholder="E-mail"
-            {...register('email', {
-              required: true,
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'invalid email address',
-              },
-            })}
-          />
+    <MainTemplate title={'Sign up'}>
+      <div className="is-flex is-flex-direction-column signup">
+        <div className="is-flex is-flex-direction-column is-justify-content-center signup__header">
+          <Image src={img} alt={'Signup banner image'} width={250} />
+          <h1>Sign up</h1>
+          <h4>I’m glad that you are joing the group of users</h4>
         </div>
-        <div className="column p-0 is-flex is-justify-content-space-between">
-          <div className="half-column">
-            {errors.firstName && <ErrorInputIcon />}
-            <input
-              type="text"
-              aria-invalid={errors.firstName ? 'true' : 'false'}
-              placeholder="First name"
-              {...register('firstName', {
-                required: true,
-                maxLength: 30,
-                minLength: 2,
-              })}
-            />
-          </div>
-          <div className="half-column">
-            {errors.secondName && <ErrorInputIcon />}
-            <input
-              type="text"
-              aria-invalid={errors.secondName ? 'true' : 'false'}
-              placeholder="Second name"
-              {...register('secondName', {
-                required: true,
-                maxLength: 30,
-                minLength: 2,
-              })}
-            />
-          </div>
-        </div>
-        <div className="column p-0">
-          {errors.username && <ErrorInputIcon />}
-          <input
-            type="text"
-            aria-invalid={errors.username ? 'true' : 'false'}
-            placeholder="Username"
-            {...register('username', {
-              required: true,
-              maxLength: 20,
-              minLength: 5,
-            })}
-          />
-        </div>
-        <div className="column p-0">
-          {errors.password && <ErrorInputIcon />}
-          <input
-            type="password"
-            aria-invalid={errors.password ? 'true' : 'false'}
-            placeholder="Password"
-            {...register('password', {
-              required: true,
-              minLength: 7,
-              maxLength: 99,
-            })}
-          />
-        </div>
-        <button
-          className="column button is-primary is-flex mx-auto mt-3"
-          type="submit"
+        <form
+          className="columns mt-4 mb-0 mx-auto is-flex is-flex-direction-column signup__form"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Create account!
-        </button>
-        <div className="my-3">
-          <span>Do you have an account? </span>
-          <Link href="/signin">Let's sign in</Link>
-        </div>
-      </form>
-    </div>
+          <div className="column p-0">
+            {errors.email && <ErrorInputIcon />}
+            <input
+              type="email"
+              aria-invalid={errors.email ? 'true' : 'false'}
+              placeholder="E-mail"
+              {...register('email', {
+                required: true,
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'invalid email address',
+                },
+              })}
+            />
+          </div>
+          <div className="column p-0 is-flex is-justify-content-space-between">
+            <div className="half-column">
+              {errors.firstName && <ErrorInputIcon />}
+              <input
+                type="text"
+                aria-invalid={errors.firstName ? 'true' : 'false'}
+                placeholder="First name"
+                {...register('firstName', {
+                  required: true,
+                  maxLength: 30,
+                  minLength: 2,
+                })}
+              />
+            </div>
+            <div className="half-column">
+              {errors.secondName && <ErrorInputIcon />}
+              <input
+                type="text"
+                aria-invalid={errors.secondName ? 'true' : 'false'}
+                placeholder="Second name"
+                {...register('secondName', {
+                  required: true,
+                  maxLength: 30,
+                  minLength: 2,
+                })}
+              />
+            </div>
+          </div>
+          <div className="column p-0">
+            {errors.username && <ErrorInputIcon />}
+            <input
+              type="text"
+              aria-invalid={errors.username ? 'true' : 'false'}
+              placeholder="Username"
+              {...register('username', {
+                required: true,
+                maxLength: 20,
+                minLength: 5,
+              })}
+            />
+          </div>
+          <div className="column p-0">
+            {errors.password && <ErrorInputIcon />}
+            <input
+              type="password"
+              aria-invalid={errors.password ? 'true' : 'false'}
+              placeholder="Password"
+              {...register('password', {
+                required: true,
+                minLength: 7,
+                maxLength: 99,
+              })}
+            />
+          </div>
+          <button
+            className="column button is-primary is-flex mx-auto mt-3"
+            type="submit"
+          >
+            Create account!
+          </button>
+          <div className="my-3">
+            <span>Do you have an account? </span>
+            <Link href="/signin">Let's sign in</Link>
+          </div>
+        </form>
+      </div>
+    </MainTemplate>
   );
 };
 
