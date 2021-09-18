028f57b53feb74c91d99bc660d60cdc810a7305a
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-09-18 14:16:24 +0200

    FE - add main content to template

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import MainTemplate from "@/templates/MainTemplate";
+import MainTemplate from '@/templates/MainTemplate';
 
 const Home: React.FC = () => {
   return (
     <MainTemplate title={'Home'}>
-      <main>
-        <a className="button is-primary">
-          Button1
-        </a>
+      <a className="button is-primary">Button1</a>
 
-        <a className="button is-primary">
-          Button2
-        </a>
-      </main>
+      <a className="button is-primary">Button2</a>
     </MainTemplate>
   );
-}
+};
 
 export default Home;
