34e2b553de585d6febeaf4d6c3d5220768ae129a
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-10-03 13:04:02 +0200

    FE - add authorization header to apollo provider

--- frontend/src/lib/withApollo.tsx
+++ frontend/src/lib/withApollo.tsx
@@ -1,20 +1,37 @@
 import {
   ApolloClient,
+  ApolloLink,
   ApolloProvider,
-  DefaultOptions,
   HttpLink,
   InMemoryCache,
 } from '@apollo/client';
 import { useRouter } from 'next/router';
 import nextWithApollo from 'next-with-apollo';
+import { useAuthToken } from '@/utils/auth-cookie';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const authMiddleware = (authToken: string) =>
+  new ApolloLink((operation, forward) => {
+    if (authToken) {
+      operation.setContext({
+        headers: {
+          authorization: `Bearer ${authToken}`,
+        },
+      });
+    }
+
+    return forward(operation);
+  });
 
 const withApollo = nextWithApollo(
   ({ initialState, headers }) => {
+    const [authToken] = useAuthToken();
     return new ApolloClient({
       ssrMode: typeof window === 'undefined',
-      link: new HttpLink({
-        uri: 'http://localhost:4000/graphql',
-      }),
+      link: authMiddleware(authToken).concat(httpLink),
       headers: {
         ...(headers as Record<string, string>),
       },
