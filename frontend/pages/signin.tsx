89d690528f51da66cd304728d147044fd3828584
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-10-07 20:31:42 +0200

    FE - secure routes

--- frontend/pages/signin.tsx
+++ frontend/pages/signin.tsx
@@ -12,6 +12,7 @@ import { popupNotification } from '@/utils/popup-notification';
 import { useLoginMutation } from '../generated';
 import { useAuthToken } from '@/hooks/useAuthToken';
 import withApollo from '@/lib/withApollo';
+import UnprotectedRoute from '@/templates/UnprotectedRoute';
 
 const Signin: React.FC = () => {
   const [authToken, setAuthToken] = useAuthToken();
@@ -49,78 +50,70 @@ const Signin: React.FC = () => {
     [reset],
   );
 
-  useEffect(() => {
-    if (authToken) {
-      Router.push('/');
-    }
-  }, []);
-
-  if (authToken) {
-    return <p>Loading...</p>;
-  }
-
   return (
-    <MainTemplate title={'Sign in'}>
-      <div className="is-flex is-flex-direction-column signin">
-        <div className="is-flex is-flex-direction-column is-justify-content-center signin__header">
-          <Image src={img} alt={'Signin banner image'} width={250} />
-          <h1>Sign in</h1>
-          <h4>Welcome back! Let's go log in to the website</h4>
-        </div>
-        <form
-          className="columns mt-4 mb-0 mx-auto is-flex is-flex-direction-column signin__form"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="column p-0">
-            {errors.email && <ErrorInputIcon />}
-            <input
-              type="email"
-              aria-invalid={errors.email ? 'true' : 'false'}
-              placeholder="E-mail"
-              {...register('email', {
-                required: true,
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'invalid email address',
-                },
-              })}
-            />
+    <UnprotectedRoute>
+      <MainTemplate title={'Sign in'}>
+        <div className="is-flex is-flex-direction-column signin">
+          <div className="is-flex is-flex-direction-column is-justify-content-center signin__header">
+            <Image src={img} alt={'Signin banner image'} width={250} />
+            <h1>Sign in</h1>
+            <h4>Welcome back! Let's go log in to the website</h4>
           </div>
-          <div className="column p-0">
-            {errors.password && <ErrorInputIcon />}
-            <input
-              type="password"
-              aria-invalid={errors.password ? 'true' : 'false'}
-              placeholder="Password"
-              {...register('password', {
-                required: true,
-                minLength: 7,
-                maxLength: 99,
-              })}
-            />
-          </div>
-          <Link href="/forgot-password">
-            <a className="is-flex is-flex-direction-row is-justify-content-flex-end">
-              Forgot password?
-            </a>
-          </Link>
-          <button
-            className={`column button is-primary is-flex mx-auto mt-3 ${
-              loading ? 'is-loading' : ''
-            }`}
-            type="submit"
-            disabled={loading}
+          <form
+            className="columns mt-4 mb-0 mx-auto is-flex is-flex-direction-column signin__form"
+            onSubmit={handleSubmit(onSubmit)}
           >
-            Log in!
-          </button>
-          <div className="my-3">
-            <span>Don't you have an account? </span>
-            <Link href="/signup">Let's sign up</Link>
-          </div>
-        </form>
-        <ToastContainer />
-      </div>
-    </MainTemplate>
+            <div className="column p-0">
+              {errors.email && <ErrorInputIcon />}
+              <input
+                type="email"
+                aria-invalid={errors.email ? 'true' : 'false'}
+                placeholder="E-mail"
+                {...register('email', {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'invalid email address',
+                  },
+                })}
+              />
+            </div>
+            <div className="column p-0">
+              {errors.password && <ErrorInputIcon />}
+              <input
+                type="password"
+                aria-invalid={errors.password ? 'true' : 'false'}
+                placeholder="Password"
+                {...register('password', {
+                  required: true,
+                  minLength: 7,
+                  maxLength: 99,
+                })}
+              />
+            </div>
+            <Link href="/forgot-password">
+              <a className="is-flex is-flex-direction-row is-justify-content-flex-end">
+                Forgot password?
+              </a>
+            </Link>
+            <button
+              className={`column button is-primary is-flex mx-auto mt-3 ${
+                loading ? 'is-loading' : ''
+              }`}
+              type="submit"
+              disabled={loading}
+            >
+              Log in!
+            </button>
+            <div className="my-3">
+              <span>Don't you have an account? </span>
+              <Link href="/signup">Let's sign up</Link>
+            </div>
+          </form>
+          <ToastContainer />
+        </div>
+      </MainTemplate>
+    </UnprotectedRoute>
   );
 };
 
