be858dd4b8ed0fb0505ebc6b89158e9bfb1ce434
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-10-07 20:31:05 +0200

    FE - fix protected route

--- frontend/src/templates/ProtectedRoute.tsx
+++ frontend/src/templates/ProtectedRoute.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { useWhoAmIQuery } from '../../generated';
 import Router from 'next/router';
+import { useLogout } from '@/hooks/useLogout';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<Props> = ({ children }: Props) => {
-  const { loading, error, data } = useWhoAmIQuery();
+  const { loading, error, data } = useWhoAmIQuery({
+    fetchPolicy: 'network-only',
+  });
+  const logout = useLogout();
 
+  // TODO: replace with loader
   if (loading) {
     return <p>Loading...</p>;
   }
 
-  if (error && !loading && !data) {
+  // if (error && !loading && !data) {
+  //   logout();
+  //   Router.push('/');
+  // }
+  //
+  // return data && !error ? <>{children}</> : <p>Loading...</p>;
+
+  if (!loading && !error?.message) {
+    return <>{children}</>;
+  }
+
+  if (!loading && error?.message) {
     Router.push('/');
   }
 
-  return data && !error ? <>{children}</> : <p>Loading...</p>;
+  return null;
 };
 
 export default ProtectedRoute;
