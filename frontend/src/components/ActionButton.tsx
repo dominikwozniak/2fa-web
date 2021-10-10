62c493645b81525d9c173162fbdc82e754b271e9
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-10-10 22:22:20 +0200

    FE - add link to action buttons

--- frontend/src/components/ActionButton.tsx
+++ frontend/src/components/ActionButton.tsx
@@ -4,22 +4,47 @@ import { IconType } from '@/types/icon.types';
 import editImg from '@/public/assets/pen.svg';
 import changePasswordImg from '@/public/assets/lock.svg';
 import twoFactorImg from '@/public/assets/bookmark.svg';
+import Link from 'next/link';
 
 interface Props {
   icon: IconType;
   text: string;
+  link: string;
 }
 
-const ActionButton: React.FC<Props> = ({ icon, text }) => {
+const ActionButton: React.FC<Props> = ({ icon, text, link }) => {
   return (
-    <button className="is-flex is-flex-direction-column is-align-items-flex-end is-justify-content-space-between button is-primary action-button">
-      <div className="is-flex is-align-items-center is-justify-content-center is-full action-button__icon">
-        {icon === IconType.pen && <Image src={editImg} alt={'Edit icon image'} width={40} height={40} />}
-        {icon === IconType.lock && <Image src={changePasswordImg} alt={'Change password icon image'} width={40} height={40} />}
-        {icon === IconType.bookmark && <Image src={twoFactorImg} alt={'Two factor icon image'} width={40} height={40} />}
-      </div>
-      <p>{text}</p>
-    </button>
+    <Link href={link}>
+      <button className="is-flex is-flex-direction-column is-align-items-flex-end is-justify-content-space-between button is-primary action-button">
+        <div className="is-flex is-align-items-center is-justify-content-center is-full action-button__icon">
+          {icon === IconType.pen && (
+            <Image
+              src={editImg}
+              alt={'Edit icon image'}
+              width={40}
+              height={40}
+            />
+          )}
+          {icon === IconType.lock && (
+            <Image
+              src={changePasswordImg}
+              alt={'Change password icon image'}
+              width={40}
+              height={40}
+            />
+          )}
+          {icon === IconType.bookmark && (
+            <Image
+              src={twoFactorImg}
+              alt={'Two factor icon image'}
+              width={40}
+              height={40}
+            />
+          )}
+        </div>
+        <p>{text}</p>
+      </button>
+    </Link>
   );
 };
 
--- frontend/src/components/ActionMenu.tsx
+++ frontend/src/components/ActionMenu.tsx
@@ -5,9 +5,9 @@ import { IconType } from '@/types/icon.types';
 const ActionMenu = () => {
   return (
     <div className="is-flex is-flex-direction-row is-justify-content-space-between action">
-      <ActionButton text={'Edit profile'} icon={IconType.pen} />
-      <ActionButton text={'Change password'} icon={IconType.lock} />
-      <ActionButton text={'Enable 2FA'} icon={IconType.bookmark} />
+      <ActionButton text={'Edit profile'} link={'/'} icon={IconType.pen} />
+      <ActionButton text={'Change password'} link={'/change-password'} icon={IconType.lock} />
+      <ActionButton text={'Enable 2FA'} link={'/'} icon={IconType.bookmark} />
     </div>
   );
 };
