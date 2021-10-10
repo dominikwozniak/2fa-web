9c971dc39be14f6cebfa6bc7a68ed712897f005a
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-10-10 10:01:44 +0200

    BE - register user-update input

---
+++ backend/src/auth/dto/user-update.input.ts
@@ -0,0 +1,19 @@
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class UserUpdateInput {
+  @Field({ nullable: true })
+  email?: string;
+
+  @Field({ nullable: true })
+  firstName?: string;
+
+  @Field({ nullable: true })
+  lastName?: string;
+
+  @Field({ nullable: true })
+  twoFactorEnabled?: boolean;
+
+  @Field({ nullable: true })
+  bio?: string;
+}
