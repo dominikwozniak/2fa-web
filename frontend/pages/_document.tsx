e3267e06e3d4ecffa762129f77ccf5d310aa98bf
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-10-15 18:48:59 +0200

    FE - next-pwa initial

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,11 +1,35 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
-class CustomDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+const APP_NAME = 'Authela';
+const APP_DESCRIPTION = 'Two-factor system';
+
+export default class extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     return await Document.getInitialProps(ctx);
   }
-}
 
-export default CustomDocument;
+  render() {
+    return (
+      <Html lang='en' dir='ltr'>
+        <Head>
+          <meta name='application-name' content={APP_NAME} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content={APP_NAME} />
+          <meta name='description' content={APP_DESCRIPTION} />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#FFFFFF' />
+
+          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='shortcut icon' href='/favicon.ico' />
+        </Head>
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
