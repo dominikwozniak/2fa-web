18bccbe8c790ffe1b6d420437b44006848f3331f
Dominik Woźniak <dominikwozniak@Hatimerias-MacBook-Pro.local>
2021-10-15 21:56:11 +0200

    BE - inline init redis

--- backend/src/redis/redis.service.ts
+++ backend/src/redis/redis.service.ts
@@ -3,18 +3,13 @@ import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
-  // TODO: inline initialize redis
-  private redis;
-
-  constructor() {
-    this.redis = new Redis(process.env.REDIS_URI);
-  }
+  private readonly redis = new Redis(process.env.REDIS_URI);
 
   async getValue(key: string): Promise<any> {
     return this.redis.get(key);
   }
 
-  async setValue(key: string, value: string): Promise<string> {
+  async setValue(key: string, value: string): Promise<any> {
     return this.redis.set(key, value, 'ex', 60 * 60 * 24);
   }
 
