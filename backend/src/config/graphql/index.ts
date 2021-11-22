983b6b12e573f047dbfa1917bd7442a809a0c63b
dominikwozniak <degie999@gmail.com>
2021-11-22 18:19:39 +0100

    BE - add origins urls

--- backend/src/config/graphql/index.ts
+++ backend/src/config/graphql/index.ts
@@ -8,7 +8,11 @@ export class GraphqlOptions implements GqlOptionsFactory {
       context: ({ req, res }) => ({ req, res }),
       autoSchemaFile: 'src/schema.gql',
       cors: {
-        origin: ['http://localhost:3000'],
+        origin: [
+          'http://localhost:3000',
+          'https://frontend-authela-deploy.vercel.app/',
+          'https://frontend-authela-deploy.vercel.app'
+        ],
         credentials: true,
       },
       debug: true,
